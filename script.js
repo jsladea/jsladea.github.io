2c86fd123cee2b6ad0fc2185894b895bb6164d20
jsladea <jaaronslade@gmail.com>
2022-03-25 15:43:40 -0600

    Added Ids to grid items through Vue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function GridItem(isHeader, text, link, imgSrc){
+function GridItem(isHeader, text, id, link, imgSrc){
     this.isHeader = isHeader;
     this.text = text;
+    this.id = id;
     this.link = link;
     this.imgSrc = imgSrc;
 }
@@ -10,23 +11,24 @@ const pythonUrl = "https://github.com/jsladea/Work-Portfolio/tree/master/Sample%
 const helpMenuUrl = "https://github.com/jsladea/Work-Portfolio/tree/master/Basic%20Help%20Menu%20in%20HTML%2C%20JavaScript";
 const basicWebsiteUrl = "https://github.com/jsladea/Work-Portfolio/tree/master/Basic%20Website%20JavaScript%2C%20HTML%2C%20Vue%20Framework";
 const schoolUrl = "https://github.com/jsladea/Work-Portfolio/tree/master/Misc.%20Schoolwork";
+const workPortfolioUrl = "https://github.com/jsladea/Work-Portfolio";
 
 const app = Vue.createApp({
     data() {
         return {
             mainGridItems: [
-                new GridItem(true, "Python", "#portfolio"),
-                new GridItem(false, "Sample Website Using Django Framework", pythonUrl, "images/portfolio-images/python-django.png"),
-                new GridItem(true, "C#", "#portfolio"),
-                new GridItem(false, "C# Windows Forms Application", cSharpUrl, "images/portfolio-images/csharp.png"),
-                new GridItem(true, "JavaScript", "#portfolio"),
-                new GridItem(false, "Example Help Menu", helpMenuUrl, "images/portfolio-images/help-menu.jpg"),
-                new GridItem(false, '"OddJobs" Sample Website Project', pythonUrl, "images/portfolio-images/oddjobs-js.png"),
-                new GridItem(false, "Example Using Vue Framework", basicWebsiteUrl, "images/portfolio-images/basic-website-js.png"),
-                new GridItem(true, "Miscellaneous Schoolwork", "#portfolio"),
-                new GridItem(false, "Schoolwork", schoolUrl, "images/portfolio-images/schoolwork.jpg"),
-                new GridItem(true, "Resume", "docs/Resume.pdf"),
-                new GridItem(false, "My Resume", "docs/Resume.pdf", "images/portfolio-images/resume.png")
+                new GridItem(true, "Python", "python-header"),
+                new GridItem(false, "Sample Website Using Django Framework", "sample-django1",pythonUrl, "images/portfolio-images/python-django.png"),
+                new GridItem(true, "C#", "c-sharp-header"),
+                new GridItem(false, "C# Windows Forms Application", "c-sharp-item1",cSharpUrl, "images/portfolio-images/csharp.png"),
+                new GridItem(true, "JavaScript", "javascript-header"),
+                new GridItem(false, "Example Help Menu", "js-item1", helpMenuUrl, "images/portfolio-images/help-menu.jpg"),
+                new GridItem(false, '"OddJobs" Sample Website Project', "js-item2", pythonUrl, "images/portfolio-images/oddjobs-js.png"),
+                new GridItem(false, "Example Using Vue Framework", "js-item3", basicWebsiteUrl, "images/portfolio-images/basic-website-js.png"),
+                new GridItem(true, "Other", "other-header"),
+                new GridItem(false, "My Resume", "resume", "docs/Resume.pdf", "images/portfolio-images/resume.png"),
+                new GridItem(false, "Entire GitHub Portfolio", "github", workPortfolioUrl, "images/portfolio-images/github.png"),
+                new GridItem(false, "Schoolwork", "schoolwork", schoolUrl, "images/portfolio-images/schoolwork.jpg"),
             ],
         };
     },
