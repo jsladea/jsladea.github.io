9dcde39d31d8eb3221396ee127c4011c9217cbc7
jsladea <jaaronslade@gmail.com>
2022-03-26 20:02:34 -0600

    Changed javascript

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -10744,8 +10744,7 @@ var Vue = (function (exports) {
     function initDev() {
         {
             {
-                console.info(`You are running a development build of Vue.\n` +
-                    `Make sure to use the production build (*.prod.js) when deploying for production.`);
+                console.info('Application deployed successfully');
             }
             initCustomFormatter();
         }
